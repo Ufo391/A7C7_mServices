d2995586b0b42a6a42f6a251b57683a43365eda9
Ufo391 <nk-west@hotmail.de>
2022-04-07 16:44:36 +0200

    Implement a derivative for AbstractOrder.

--- SharedLibrary/TestPackages.Utils/Bookkeepings/AbstractOrder.cs
+++ SharedLibrary/TestPackages.Utils/Bookkeepings/AbstractOrder.cs
@@ -14,9 +14,9 @@ namespace TestPackages.Bookkeepings
             BEARISCH, BULLISH
         }
 
-        // Konstruktor
+        // Constructor
         public AbstractOrder(string id, string market, ORDER_STATUS status, DirectionType direction, 
-            DateTime openDate, double volume, double openPrice, string pair)
+            DateTime openDate, double volume, double openPrice)
         {
             ExternId = id;
             Status = status;
@@ -24,12 +24,11 @@ namespace TestPackages.Bookkeepings
             OpenDate = openDate;
             Volume = volume;
             OpenPrice = openPrice;            
-            TradingPair = pair;
             Market = market;
             InternalId = Guid.NewGuid();
         }
 
-        // Attribute
+        // Attributes
         public string ExternId { get; private set; } // Z.B. ID von MetaTrader
         public Guid InternalId { get; private set; }
 
@@ -62,40 +61,14 @@ namespace TestPackages.Bookkeepings
         public double ClosePrice { get; private set; }        
         public double StopLoss { get; set; }
         public double TakeProfit { get; set; }
-        public string TradingPair { get; private set; }
 
-        // Funktionen
+        // Functions
         public override string ToString()
         {
-            return $"[{ExternId}] | OpenDate: {OpenDate} | {Enum.GetName(typeof(ORDER_STATUS), Status)} | {Market} | {Enum.GetName(typeof(DirectionType), Direction)} | OpenPrice: {OpenPrice} | Pair: {TradingPair}";
+            return $"[{ExternId}] | OpenDate: {OpenDate} | {Enum.GetName(typeof(ORDER_STATUS), Status)} | {Market} | {Enum.GetName(typeof(DirectionType), Direction)} | OpenPrice: {OpenPrice}";
         }
 
-        public override bool Equals(object obj)
-        {
-            if(obj is AbstractOrder == false)
-            {
-                return false;
-            }
-            var order = (AbstractOrder)obj;
-            return order.OpenDate.Equals(OpenDate) && order.ExternId.Equals(ExternId) && order.Market.Equals(Market) && order.Direction.Equals(Direction) && order.TradingPair.Equals(TradingPair) && order.OpenPrice == OpenPrice;
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int result = 126345;                
-                result *= OpenDate.GetHashCode() + 4321;
-                result *= ExternId.GetHashCode() + 4321;
-                result *= Market.GetHashCode() + 4321;
-                result *= Direction.GetHashCode() + 4321;
-                result *= TradingPair.GetHashCode() + 4321;
-                result *= OpenPrice.GetHashCode() + 4321;
-                return result;
-            }
-        }
-
-        // Methoden
+        // Methods
         public void RefreshOrder(AbstractOrder order)
         {
             OpenDate = order.OpenDate;
@@ -105,6 +78,43 @@ namespace TestPackages.Bookkeepings
             StopLoss = order.StopLoss;
             TakeProfit = order.TakeProfit;
             Status = order.Status;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is AbstractOrder order &&
+                   ExternId == order.ExternId &&
+                   InternalId.Equals(order.InternalId) &&
+                   status == order.status &&
+                   Status == order.Status &&
+                   Market == order.Market &&
+                   Direction == order.Direction &&
+                   OpenDate == order.OpenDate &&
+                   CloseDate == order.CloseDate &&
+                   Volume == order.Volume &&
+                   OpenPrice == order.OpenPrice &&
+                   ClosePrice == order.ClosePrice &&
+                   StopLoss == order.StopLoss &&
+                   TakeProfit == order.TakeProfit;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(ExternId);
+            hash.Add(InternalId);
+            hash.Add(status);
+            hash.Add(Status);
+            hash.Add(Market);
+            hash.Add(Direction);
+            hash.Add(OpenDate);
+            hash.Add(CloseDate);
+            hash.Add(Volume);
+            hash.Add(OpenPrice);
+            hash.Add(ClosePrice);
+            hash.Add(StopLoss);
+            hash.Add(TakeProfit);
+            return hash.ToHashCode();
         }
     }
 }
---
+++ SharedLibrary/TestPackages.Utils/Bookkeepings/ForexOrder.cs
@@ -0,0 +1,31 @@
+﻿using TestPackages.Bookkeepings;
+
+namespace TestPackages.Utils.Bookkeepings
+{
+    public class ForexOrder : AbstractOrder
+    {
+        public string TradingPair { get; init; }
+
+        public ForexOrder(string id, string market, ORDER_STATUS status, DirectionType direction, DateTime openDate, double volume, double openPrice, string tradingPair) : base(id, market, status, direction, openDate, volume, openPrice)
+        {
+            TradingPair = tradingPair;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Pair: {TradingPair}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ForexOrder order &&
+                   base.Equals(obj) &&
+                   TradingPair == order.TradingPair;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), TradingPair);
+        }
+    }
+}
